024d5b5a9d01f5c776e810a032b22f1b9711d899
clairox <claireq620@gmail.com>
2024-07-25 19:34:02 -0400

    feat: use middleware to check customerAccessToken

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export const middleware = (request: NextRequest) => {
+	const pathname = request.nextUrl.pathname
+	if (pathname === '/account') {
+		return NextResponse.rewrite(new URL('/account/settings', request.url))
+	}
+	if (pathname.startsWith('/api/customer')) {
+		const customerAccessToken = request.cookies.get('customerAccessToken')
+		if (!customerAccessToken) {
+			return NextResponse.json(
+				{ success: false, data: {}, error: { code: '', message: '' } },
+				{ status: 401 }
+			)
+		}
+
+		return NextResponse.next()
+	}
+
+	return NextResponse.next()
+}
