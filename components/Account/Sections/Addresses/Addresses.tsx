2802c38b89c9626907deb329c761650c573353fa
clairox <claireq620@gmail.com>
2024-07-25 01:19:31 -0400

    feat: use regular icon weights in account page

--- components/Account/Sections/Addresses/Addresses.tsx
+++ components/Account/Sections/Addresses/Addresses.tsx
@@ -6,7 +6,7 @@ const Addresses = () => {
 		<div>
 			<div className="w-full">
 				<h1 className="flex items-end gap-4 pl-5 pb-6 h-32 border-r border-b border-black text-4xl font-bold">
-					<House size={40} weight="bold" />
+					<House size={40} weight="regular" />
 					Shipping Addresses
 				</h1>
 			</div>
--- components/Account/Sections/ChangePassword/ChangePassword.tsx
+++ components/Account/Sections/ChangePassword/ChangePassword.tsx
@@ -7,7 +7,7 @@ const ChangePassword = () => {
 		<div>
 			<div className="w-full">
 				<h1 className="flex items-end gap-4 pl-5 pb-6 h-32 border-r border-b border-black text-4xl font-bold">
-					<Lock size={40} weight="bold" />
+					<Lock size={40} weight="regular" />
 					Change Password
 				</h1>
 			</div>
