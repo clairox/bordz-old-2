90dadeb4764935888a2420706c323dd66175bc6e
clairox <claireq620@gmail.com>
2024-07-25 19:31:43 -0400

    feat: justify content to flex-start

--- components/FormResponseBox/FormErrorBox/FormErrorBox.tsx
+++ components/FormResponseBox/FormErrorBox/FormErrorBox.tsx
@@ -1,10 +1,11 @@
 import { WarningCircle } from '@phosphor-icons/react'
 import React, { PropsWithChildren } from 'react'
 
+// TODO: !! Justify start
 const FormErrorBox: React.FunctionComponent<PropsWithChildren> = ({ children }) => {
 	return (
 		<div
-			className="flex flex-row gap-4 justify-center items-center px-4 w-full h-[75px] bg-red-200 text-sm"
+			className="flex flex-row gap-4 justify-start items-center px-4 w-full h-[75px] bg-red-200 text-sm"
 			data-testid="formErrorBox"
 		>
 			<WarningCircle size={50} weight="regular" />
--- components/FormResponseBox/FormSuccessBox/FormSuccessBox.tsx
+++ components/FormResponseBox/FormSuccessBox/FormSuccessBox.tsx
@@ -4,7 +4,7 @@ import React, { PropsWithChildren } from 'react'
 const FormSuccessBox: React.FunctionComponent<PropsWithChildren> = ({ children }) => {
 	return (
 		<div
-			className="flex flex-row gap-4 justify-center items-center px-4 w-full h-[75px] bg-green-200 text-sm"
+			className="flex flex-row gap-4 justify-start items-center px-4 w-full h-[75px] bg-green-200 text-sm"
 			data-testid="formSuccessBox"
 		>
 			<Check size={50} weight="regular" />
