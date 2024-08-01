a5e63c75fe77bf6653ab970053dc2f8b24104bf9
Claire Q <33704909+clairox@users.noreply.github.com>
2024-08-01 12:25:16 -0400

    feat: update CartAction.tsx

--- components/Header/HeaderActions/CartAction/CartAction.tsx
+++ components/Header/HeaderActions/CartAction/CartAction.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { useState } from 'react'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/Popover'
-import MiniLoginForm from '@/components/Forms/MiniLoginForm'
 import HeaderButton from '@/components/Header/HeaderAction'
 import { BagSimple } from '@phosphor-icons/react/dist/ssr'
 
@@ -14,9 +13,7 @@ const CartAction = () => {
 					<BagSimple size={28} weight={open ? 'fill' : 'light'} />
 				</HeaderButton>
 			</PopoverTrigger>
-			<PopoverContent className="pt-3 border-t-0 border-r-0">
-				<MiniLoginForm />
-			</PopoverContent>
+			<PopoverContent className="pt-3 border-t-0 border-r-0"></PopoverContent>
 		</Popover>
 	)
 }
