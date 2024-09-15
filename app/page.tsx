ee5f7e51a58a5273f6631f5db96fcbe6ce6fca3d
clairox <claireq620@gmail.com>
2024-09-15 12:48:52 -0400

    feat: revert homepage to empty component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-'use client'
-
-import DateSelect from '@/components/UI/DateSelect'
-import { useState } from 'react'
-
-const Page = () => {
-    const [date, setDate] = useState<Date | undefined>(undefined)
-
-    return (
-        <main>
-            <p>Date: {date?.toUTCString() ?? 'Invalid Date'}</p>
-            <DateSelect value={date} onChange={setDate} />
-        </main>
-    )
+const Page = async () => {
+    return <main></main>
 }
 
 export default Page
